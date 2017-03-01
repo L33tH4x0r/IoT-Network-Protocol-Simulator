88c50d465e5544aebdc3850bf5194b043a082ef9
Austin <austin@dringo.io>
2017-02-28 18:22:14 -0800

    Removed an debugging addition

--- ClientClass.py
+++ ClientClass.py
@@ -25,7 +25,7 @@ class Client:
         # Connect client to server
         self.client_socket.connect((server_IP, int(server_port)))
         # Add MAC address of current machine
-        self.mac = get_mac() + 1
+        self.mac = get_mac()
         # create error log
         self.error_log = open('error.log', 'a')
 
